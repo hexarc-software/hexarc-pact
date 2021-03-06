2b6ec7172b60b76a7007a210d8889c1e67de03df
Max Koverdyaev <shadeglare@gmail.com>
2021-03-06 14:04:39 +0300

    more predefined array like types

--- Hexarc.Pact.Protocol/TypeProviders/ArrayLikeTypeProvider.cs
+++ Hexarc.Pact.Protocol/TypeProviders/ArrayLikeTypeProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Hexarc.Pact.Protocol.Types;
 
@@ -8,13 +9,26 @@ namespace Hexarc.Pact.Protocol.TypeProviders
     /// <summary>
     /// The stock array-like type provider used in the Hexarc Pact protocol.
     /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public sealed class ArrayLikeTypeProvider
     {
-        private ArrayLikeType EnumerableOfT { get; } = new(typeof(IEnumerable<>));
+        private ArrayLikeType IEnumerableOfT { get; } = new(typeof(IEnumerable<>));
+
+        private ArrayLikeType ICollectionOfT { get; } = new(typeof(ICollection<>));
+
+        private ArrayLikeType IReadOnlyCollection { get; } = new(typeof(IReadOnlyCollection<>));
 
         private ArrayLikeType ListOfT { get; } = new(typeof(List<>));
 
+        private ArrayLikeType IListOfT { get; } = new(typeof(IList<>));
+
+        private ArrayLikeType IReadOnlyListOfT { get; } = new(typeof(IReadOnlyList<>));
+
         private ArrayLikeType HashSetOf { get; } = new(typeof(HashSet<>));
+
+        private ArrayLikeType ISetOf { get; } = new(typeof(ISet<>));
+
+        private ArrayLikeType IReadOnlySetOfT { get; } = new(typeof(IReadOnlySet<>));
 
         /// <summary>
         /// Gets the registered array-like type ids.
@@ -35,9 +49,15 @@ namespace Hexarc.Pact.Protocol.TypeProviders
         /// <returns>The registered array-like type collection.</returns>
         public IEnumerable<ArrayLikeType> Enumerate()
         {
-            yield return this.EnumerableOfT;
+            yield return this.IEnumerableOfT;
+            yield return this.ICollectionOfT;
+            yield return this.IReadOnlyCollection;
             yield return this.ListOfT;
+            yield return this.IListOfT;
+            yield return this.IReadOnlyListOfT;
             yield return this.HashSetOf;
+            yield return this.ISetOf;
+            yield return this.IReadOnlySetOfT;
         }
     }
 }
