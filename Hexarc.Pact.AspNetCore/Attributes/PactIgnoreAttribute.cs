aa542cfb08c0a542fbe741a9a96f4f07873ffec4
Max Koverdyaev <shadeglare@gmail.com>
2021-06-15 16:48:14 +0300

    [docs] describe asp.net additional attributes

--- Hexarc.Pact.AspNetCore/Attributes/PactIgnoreAttribute.cs
+++ Hexarc.Pact.AspNetCore/Attributes/PactIgnoreAttribute.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Hexarc.Pact.AspNetCore.Attributes
 {
+    /// <summary>
+    /// Prevents a method or class from being included to a Pact schema.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
     public sealed class PactIgnoreAttribute : Attribute { }
 }
--- Hexarc.Pact.AspNetCore/Attributes/PactScopeAttribute.cs
+++ Hexarc.Pact.AspNetCore/Attributes/PactScopeAttribute.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Hexarc.Pact.AspNetCore.Attributes
 {
+    /// <summary>
+    /// Allows to put a Web API controller class to a specific scope
+    /// which can be used as a filter during API schema fetching.
+    /// </summary>
     [AttributeUsage(validOn: AttributeTargets.Class, AllowMultiple = true)]
     public sealed class PactScopeAttribute : Attribute
     {
